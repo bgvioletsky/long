f19b83c661cdb3fd460294e04836bd07858b3dbe
bgcode <violetcode@qq.com>
2024-09-25 20:01:07 +0800

    s

--- QX/long.js
+++ QX/long.js
@@ -1,5 +1,5 @@
 
-const url = "https://cdn.jsdelivr.net/gh/bgvioletsky/long@0.0.5/index.html";
+const url = "https://cdn.jsdelivr.net/gh/bgvioletsky/long@0.0.6/index.html";
 const myRequest = {
     url: url
 };
